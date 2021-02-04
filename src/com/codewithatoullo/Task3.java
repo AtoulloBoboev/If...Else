885d645f1fc8ce9c7db41517b4d4367fbd2c66eb
Atoullo Boboev <atoullo.boboev@gmail.com>
2021-02-04 12:25:16 -0500

    if...else statement (d2)

---
+++ src/com/codewithatoullo/Task3.java
@@ -0,0 +1,27 @@
+package com.codewithatoullo;
+
+/** Реализовать Task №2, используя цикл while.
+ *
+ */
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner it = new Scanner(System.in);
+        int a, b;
+        System.out.print("Enter the first number: ");
+        a = it.nextInt();
+        System.out.print("Enter the second number: ");
+        b = it.nextInt();
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        }
+        int t = a + 1;
+        while (t < b) {
+            if (t % 5 == 0 && t % 10 != 0)
+                System.out.println(t);
+            t++;
+        }
+    }
+}
